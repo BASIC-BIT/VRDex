728355baecda629ea0d757ebe1f60f4f768eb8ae
BASICBIT <basic.bit.1001@gmail.com>
2026-05-28 11:58:47 -0400

    infra: use remote state for SES Terraform

--- infra/terraform/ses/versions.tf
+++ infra/terraform/ses/versions.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.6.0"
 
+  backend "s3" {
+    bucket         = "vrdex-terraform-state"
+    key            = "ses/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "vrdex-terraform-locks"
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
